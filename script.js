d6696f94feee7b6cb717a2ed4597d1ba2c013bc1
Md sifullah shakh <106006907+sifullahshakh@users.noreply.github.com>
2024-02-08 13:32:56 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach(sec =>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAnttribute("id");
+
+    if(top >= offset && top < offset + height){
+          navLinks.forEach(links => {
+              links.classList.remove("active")
+              document.querySelectorAll("header nav a [href*= " + id + " ]").classList.add("active");
+          })
+    }
+  })
+}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("fa-x");
+  navbar.classList.toggle("active");
+}
